82a7de55ffd24cc4307a44c9a1f84b354211701b
divanbeigi <hosseindbk@yahoo.com>
2023-07-12 14:46:18 +0330

    v1.1

--- app/Models/Dashboard/Permission.php
+++ app/Models/Dashboard/Permission.php
@@ -6,7 +6,7 @@ use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class permission extends Model
+class Permission extends Model
 {
     use HasFactory;
     use Sluggable;
--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
-use App\Models\Dashboard\permission;
+use App\Models\Dashboard\Permission;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
 
